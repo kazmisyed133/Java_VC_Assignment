51ed2e1abc1cae20e1877f560bd787487f314670
kazmisyed133 <syed22000@student.hamk.fi>
2024-01-06 14:47:51 +0200

    Create Scanner to get input from user.

--- AgeProgram_Java/src/AgeProgram.java
+++ AgeProgram_Java/src/AgeProgram.java
@@ -4,6 +4,10 @@ public class AgeProgram
 {
     public static void main(String[] args)
     {
-       
+    	Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your birth year: ");
+
+        int birthYear = scanner.nextInt();
     }
 }
